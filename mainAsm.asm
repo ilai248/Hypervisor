f7478134033b8cb54c10b4a53989b47c801cf8da
ilai248 <62371076+ilai248@users.noreply.github.com>
2025-05-29 21:24:01 +0300

    Update mainAsm.asm

--- mainAsm.asm
+++ mainAsm.asm
@@ -1,11 +1,9 @@
-PUBLIC AsmEnableVmxOperation
-PUBLIC AsmVmxon
-PUBLIC AsmVmptrld
-PUBLIC AsmRdmsrWord
-PUBLIC AsmReadRegister
+PUBLIC enableVmx
+PUBLIC readMsrWord
+PUBLIC readReg
 .code _text
 
-AsmEnableVmxOperation PROC PUBLIC
+enableVmx PROC PUBLIC
 	PUSH RAX			    ; Save the state
 	
 	XOR RAX, RAX			; Clear the RAX
@@ -16,33 +14,14 @@ AsmEnableVmxOperation PROC PUBLIC
 	
 	POP RAX			     	; Restore the state
 	RET
-AsmEnableVmxOperation ENDP
+enableVmx ENDP
 
-AsmVmxon PROC PUBLIC
-	VMXON QWORD PTR [RCX]	; The operand must be in memory.
-	JC carry
-	JZ zero
-	MOV RAX, 0
-	RET
-	carry:
-	MOV RAX, 1
-	RET
-	zero:
-	MOV RAX, 2
-	RET
-AsmVmxon ENDP
-
-AsmVmptrld PROC PUBLIC
-	VMPTRLD QWORD PTR [RCX]	; The operand must be in memory.
-	RET
-AsmVmptrld ENDP
-
-AsmRdmsrWord PROC PUBLIC
+readMsrWord PROC PUBLIC
 	RDMSR					; assumes the MSR needed is already in RCX.
 	RET						; return EAX (lower 32 bits of MSR)
-AsmRdmsrWord ENDP
+readMsrWord ENDP
 
-AsmReadRegister PROC PUBLIC
+readReg PROC PUBLIC
 	CMP RCX, 0
 	JE ReadCR0
 	CMP RCX, 1
@@ -70,6 +49,6 @@ AsmReadRegister PROC PUBLIC
 	ReadCR4:
 	MOV RAX, CR4
 	RET
-AsmReadRegister ENDP
+readReg ENDP
 
 END
